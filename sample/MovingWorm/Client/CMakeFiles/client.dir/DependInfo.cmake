0844a9a423c9c9e627a46c71bb60fd01856d7e4f
KimSangYeon-DGU <sy0814k@gmail.com>
2017-12-03 19:35:12 +0900

    움직이는 지렁이 코드 추가

---
+++ sample/MovingWorm/Client/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/Desktop/ICPC/2017-2-CECD-ButterflyEffect-2/1203_2/Client/Client.cpp" "/home/ubuntu/Desktop/ICPC/2017-2-CECD-ButterflyEffect-2/1203_2/Client/CMakeFiles/client.dir/Client.cpp.o"
+  "/home/ubuntu/Desktop/ICPC/2017-2-CECD-ButterflyEffect-2/1203_2/Client/PracticalSocket.cpp" "/home/ubuntu/Desktop/ICPC/2017-2-CECD-ButterflyEffect-2/1203_2/Client/CMakeFiles/client.dir/PracticalSocket.cpp.o"
+  "/home/ubuntu/Desktop/ICPC/2017-2-CECD-ButterflyEffect-2/1203_2/Client/Worms.cpp" "/home/ubuntu/Desktop/ICPC/2017-2-CECD-ButterflyEffect-2/1203_2/Client/CMakeFiles/client.dir/Worms.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sample/MovingWorm/Client/Client.cpp
@@ -0,0 +1,258 @@
+#include "opencv2/opencv.hpp"
+#include "config.h"
+#include "PracticalSocket.h"      // For UDPSocket and SocketException
+#include <iostream>               // For cout and cerr
+#include <cstdlib>                // For atoi()
+#include <cstring>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctime>
+#include <unistd.h>
+#include <pthread.h>
+#include <sstream>
+#include <vector>
+#include "Point.h"
+#include "Worms.h"
+using namespace std;
+using namespace cv;
+
+#define PORT_NUM "9000"
+#define ADDR "127.0.0.1"
+//#define ADDR "13.124.244.68"
+#define BUFFER_SIZE 2000
+#define PI 3.141592
+#define OBJECT_SIZE 30
+
+vector<UserPoint> keyPoints;
+pthread_mutex_t count_mutex;
+int sock;
+int point[4];
+int cur_X, cur_Y;
+Worms worms;
+float theta;
+int key_Idx = 0;
+
+//이미지 회전 함수
+Mat ImageRotate(Mat src, double degree, Point2d base){
+	Mat dst = src.clone();
+	Mat rot = getRotationMatrix2D(base, degree, 1);
+	warpAffine(src, dst, rot, src.size());
+	return move(dst);
+}
+
+//이미지 그리기 함수
+void DrawImage(Mat src, Mat frame, Point p, double degree, double size){
+	Mat dst;
+	Mat mask;
+	if(degree != 0){
+		//이미지 각도에 맞게 회전
+		Point2d mid(src.cols/size, src.rows/size);
+		dst = ImageRotate(src, degree, mid);
+	}
+
+	//프레임에 이미지 붙이기
+	Rect target_roi(p.x, p.y, dst.cols/size, dst.rows/size);
+	cv::resize(dst, dst, Size(target_roi.width, target_roi.height));
+	if(dst.channels() == 4){
+		vector<Mat> ch;
+		split(dst, ch);
+		mask = 255 - ch[3].clone();
+		mask.convertTo(mask, CV_32FC1, 1.0/255.0);
+		ch.erase(ch.begin()+3);
+		merge(ch, dst);
+	}else{
+		if(dst.channels() == 3){
+			cvtColor(dst, dst, COLOR_BGR2GRAY);
+		}
+		dst.convertTo(mask, CV_32FC1, 1.0/255.0);
+	}
+
+	for(int i = 0; i < dst.rows; i++){
+		for(int j = 0; j < dst.cols; j++){
+			float blending_coeff = mask.at<float>(i, j);
+			Vec3b v1 = frame.at<Vec3b>(i + target_roi.y, j + target_roi.x);
+			Vec3b v2;
+			if(dst.channels() == 1){
+				int v = dst.at<uchar>(i, j);
+				v2 = (v, v, v);
+			}else{
+				v2 = dst.at<Vec3b>(i, j);
+			}
+			Vec3f v1f(v1[0], v1[1], v1[2]);
+			Vec3f v2f(v2[0], v2[1], v2[2]);
+			Vec3f r = v1f * blending_coeff + (1.0 - blending_coeff) * v2f;
+			frame.at<Vec3b>(i + target_roi.y, j + target_roi.x) = r;
+		}
+	}
+}
+
+//drawing worms on the frame
+void drawing(Mat frame, Mat image){
+	vector<Pt> bodys = worms.getBody();
+	for(int i = bodys.size() - 1;i >= 0; i--){
+		if(i == 0)	
+			//circle(frame, Point(bodys[i].x, bodys[i].y), OBJECT_SIZE, Scalar(255, 255, 255), -1);
+			DrawImage(image, frame, Point(bodys[i].x-(image.cols/4), bodys[i].y-(image.rows/4)), 1.0, 1.5);
+		else
+			circle(frame, Point(bodys[i].x, bodys[i].y), OBJECT_SIZE, Scalar(0, 255, 0), -1);
+		cout << "body:" << i << " (" << bodys[i].x << "," << bodys[i].y << ")" << endl;
+	}
+			
+}
+
+//사용자의 오른쪽 손과 몸통 좌표의 차를 이용하여 각도를 구한다.
+float getDegree(int idx){
+	float degree, dx, dy, radian;
+	if(!keyPoints[idx].rightHand.x && !keyPoints[idx].rightHand.y && !keyPoints[idx].body.x && !keyPoints[idx].body.y){
+		key_Idx--;
+		return theta;
+	}else{
+		dx = keyPoints[idx].rightHand.x - keyPoints[idx].body.x;
+		dy = keyPoints[idx].rightHand.y - keyPoints[idx].body.y;
+		radian = atan(dy/dx);
+		degree = (radian*180)/PI;
+		return degree;
+	}
+}
+
+void *DataHandler(void *ptr){
+    while(true){
+        char server_reply[BUFFER_SIZE];
+        memset(server_reply, '\0', sizeof(server_reply));
+        int byte_Num;
+        if(byte_Num = recv(sock, server_reply, BUFFER_SIZE, 0) < 0){
+			cout << "receive error!!" << endl;
+        }
+        //parsing
+        cout <<"data :" << server_reply << endl;
+        string str(server_reply);
+        string token;
+        stringstream stream(str);
+        int i = 0;
+        stream >> token;
+        int size = std::stoi(token);
+        for(i=0; i < size; i++){
+            UserPoint userPoint;
+            stream >> token;
+            userPoint.body.x = std::stoi(token);
+            stream >> token;
+            userPoint.body.y = std::stoi(token);
+            stream >> token;
+            userPoint.rightHand.x = std::stoi(token);
+            stream >> token;
+            userPoint.rightHand.y = std::stoi(token);
+            keyPoints.push_back(userPoint);
+        }
+		/*
+        for(i=0; i<size; i++){
+            cout << "keyPoints:" << (i+1) << endl;
+            cout << "body:" << keyPoints[i].body.x <<"," << keyPoints[i].body.y<<endl;
+            cout << "rightHand:" << keyPoints[i].rightHand.x << "," << keyPoints[i].rightHand.y<<endl;
+        }*/
+
+		//theta = getDegree(key_Idx++); 오픈포즈 속도 문제가 해결되면 이 코드를 사용하면 객체가 이동함.
+		theta = 30;
+    }
+}
+
+void init(){
+	cur_X = 100;
+	cur_Y = 100;
+	theta = 0;
+}
+
+int main() {
+	init();
+	int d = 1;
+    string servAddress = ADDR; // First arg: server address
+    unsigned short servPort = Socket::resolveService(PORT_NUM, "udp");
+    try {
+		/*tcp 연결*/
+		/*thread 생성부분 ... thread에서는 recv하여 전역변수 수정*/
+        //tcp
+        sockaddr_in server;
+        pthread_t client_thread;
+
+        //소켓 생성
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if(sock == -1){
+            cout << "소켓 생성에 실패하였습니다." << endl;
+            exit(1);
+        }
+        cout << "TCP소켓을 생성하였습니다." << endl;
+
+        //소켓 준비
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = inet_addr(ADDR);
+        server.sin_port = htons(stoi(PORT_NUM));
+
+        //커넥트
+        if(connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0){
+            cout<< "커넥트 에러" << endl;
+            exit(1);
+        }
+        cout << "연결되었습니다." << endl;
+        pthread_create(&client_thread, NULL, &DataHandler, NULL);
+
+        //udp
+        UDPSocket sock;
+        int jpegqual =  ENCODE_QUALITY; // Compression Parameter
+
+        Mat frame, send;
+        vector < uchar > encoded;
+        VideoCapture cap(0); // Grab the camera
+        namedWindow("send", CV_WINDOW_AUTOSIZE);
+        if (!cap.isOpened()) {
+            cerr << "OpenCV Failed to open camera";
+            exit(1);
+        }
+        clock_t last_cycle = clock();
+
+		Mat image = imread("images/head.png", -1);
+		if(image.empty()){
+			cout << "이미지 불러오기 실패" << endl;
+			return 0;
+		}
+        while (1) {
+            cap >> frame;
+            if(frame.size().width==0)continue;//simple integrity check; skip erroneous data...
+            resize(frame, send, Size(FRAME_WIDTH, FRAME_HEIGHT), 0, 0, INTER_LINEAR);
+            vector < int > compression_params;
+            compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+            compression_params.push_back(jpegqual);
+            imencode(".jpg", send, encoded, compression_params);
+
+            //drawing worms
+            worms.setTheta(theta);
+            worms.move();
+            drawing(send, image);
+            imshow("send", send);
+           
+            int total_pack = 1 + (encoded.size() - 1) / PACK_SIZE;
+
+            int ibuf[1];
+            ibuf[0] = total_pack;
+            sock.sendTo(ibuf, sizeof(int), servAddress, servPort);
+
+            for (int i = 0; i < total_pack; i++)
+                sock.sendTo( & encoded[i * PACK_SIZE], PACK_SIZE, servAddress, servPort);
+
+            waitKey(FRAME_INTERVAL);
+
+            clock_t next_cycle = clock();
+            double duration = (next_cycle - last_cycle) / (double) CLOCKS_PER_SEC;
+			//cout << "\teffective FPS:" << (1 / duration) << " \tkbps:" << (PACK_SIZE * total_pack / duration / 1024 * 8) << endl;
+
+            last_cycle = next_cycle;
+        }
+			//Destructor closes the socket
+
+    }catch (SocketException & e) {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+    return 0;
+}

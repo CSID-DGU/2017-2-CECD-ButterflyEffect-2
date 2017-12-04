c4fcbc0f79dbef733198198f8a7f5d913ad34a95
Sunyeop Na <coinnip@gmail.com>
2017-12-04 15:14:59 +0900

    imshow 수정

--- sample/MovingWorm/Server/Server.cpp
+++ sample/MovingWorm/Server/Server.cpp
@@ -415,8 +415,9 @@ public:
                             + std::to_string(handHeatMaps[1].getSize(3)) + "]");
                 }
 
-                // Display rendered output image
-                cv::imshow("User worker GUI", datumsPtr->at(0).cvOutputData);
+              (User worker GUI:1763): Gtk-WARNING **: cannot open display: 
+  // Display rendered output image
+  //              cv::imshow("User worker GUI", datumsPtr->at(0).cvOutputData);
                 // Display image and sleeps at least 1 ms (it usually sleeps ~5-10 msec to display the image)
                 const char key = (char)cv::waitKey(1);
                 if (key == 27)
